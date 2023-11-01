8aa8529768ee61bb966f512fd7ee58ed402702a7
Andrew Houghton <houghtonandrew0@gmail.com>
2023-11-02 00:06:39 +1100

    Setting up page layout

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # andrew-houghton.github.io
 WikiRiver Offline Tool
+
+
+Functions on page:
+    Search for gauges
+    Copy gauge id
+    Insert current location with location permission
+    Checkbox for plaintext
+    Insert client version
+    Paste received string into box and view data
+
+Data to store:
+    http://localhost:8000/email/gauges.js
+    weather icons
+    decoding js script
+
+Note: should all be one page for simplicity
+
+Toggle button at top to switch modes.
+
+Parser:
+    Textarea
+    Button below
+    When run it fills the area below the button with formatted data.
+
+CSS docs https://getmdl.io/components/index.html
---
+++ assets/decode.js
@@ -0,0 +1,117 @@
+function base64ToArrayBuffer(base64) {
+    var binaryString = atob(base64);
+    var bytes = new Uint8Array(binaryString.length);
+    for (var i = 0; i < binaryString.length; i++) {
+        bytes[i] = binaryString.charCodeAt(i);
+    }
+    return bytes.buffer;
+}
+
+function extractNum(data, numBytes) {
+    data.ptr += numBytes;
+    var selectedBytes = data.bytes.slice(data.ptr-numBytes, data.ptr);
+
+    var val;
+    if (numBytes == 1) {
+        var val = new Uint8Array(selectedBytes)[0];
+    } else if (numBytes == 2) {
+        var val = new Uint16Array(selectedBytes)[0];
+    } else {
+        var val = new Uint32Array(selectedBytes)[0];
+    }
+    if (val == 2 ** (numBytes*8) - 1) return null;
+    return val;
+}
+
+function roundTwoDp(num) {
+    return Math.round((num + Number.EPSILON) * 100) / 100;
+}
+
+var symbolLookup = [
+    ['01d.svg', 'clear sky'],
+    ['02d.svg', 'fair'],
+    ['03d.svg', 'partly cloudy'],
+    ['04.svg', 'cloudy'],
+    ['05d.svg', 'rain showers'],
+    ['06d.svg', 'rain showers and thunder'],
+    ['07d.svg', 'sleet showers'],
+    ['08d.svg', 'snow showers'],
+    ['09.svg', 'rain'],
+    ['10.svg', 'heavy rain'],
+    ['11.svg', 'heavy rain and thunder'],
+    ['12.svg', 'sleet'],
+    ['13.svg', 'snow'],
+    ['14.svg', 'snow and thunder'],
+    ['15.svg', 'fog'],
+    ['20d.svg', 'sleet showers and thunder'],
+    ['21d.svg', 'snow showers and thunder'],
+    ['22.svg', 'rain and thunder'],
+    ['23.svg', 'sleet and thunder'],
+    ['24d.svg', 'light rain showers and thunder'],
+    ['25d.svg', 'heavy rain showers and thunder'],
+    ['26d.svg', 'light sleet showers and thunder'],
+    ['27d.svg', 'heavy sleet showers and thunder'],
+    ['28d.svg', 'light snow showers and thunder'],
+    ['29d.svg', 'heavy snow showers and thunder'],
+    ['30.svg', 'light rain and thunder'],
+    ['31.svg', 'light sleet and thunder'],
+    ['32.svg', 'heavy sleet and thunder'],
+    ['33.svg', 'light snow and thunder'],
+    ['34.svg', 'heavy snow and thunder'],
+    ['40d.svg', 'light rain showers'],
+    ['41d.svg', 'heavy rain showers'],
+    ['42d.svg', 'light sleet showers'],
+    ['43d.svg', 'heavy sleet showers'],
+    ['44d.svg', 'light snow showers'],
+    ['45d.svg', 'heavy snow showers'],
+    ['46.svg', 'light rain'],
+    ['47.svg', 'light sleet'],
+    ['48.svg', 'heavy sleet'],
+    ['49.svg', 'light snow'],
+    ['50.svg', 'heavy snow'],
+]
+
+function decode(text){
+    // data holds a byte array and a reference to the last position read in the array
+    var data = { ptr: 0, bytes: base64ToArrayBuffer(text) };
+    var output = {};
+
+    var baseTimeS = extractNum(data, 4);
+    output.baseTime = new Date(baseTimeS * 1000);
+    output.gaugeId = extractNum(data, 2);
+    var gaugeLen = extractNum(data, 1);
+    var weatherLen = extractNum(data, 1);
+
+    output.weatherItems = [];
+    for (let i = 0; i < weatherLen; i++) {
+        var item = {};
+        item.rainfall = roundTwoDp(extractNum(data, 2) / 10);
+        item.max = roundTwoDp(extractNum(data, 2) / 10 - 100);
+        item.min = roundTwoDp(extractNum(data, 2) / 10 - 100);
+        item.wind = roundTwoDp(extractNum(data, 2) / 10);
+        var symbolRaw = extractNum(data, 1);
+        item.symbolUrl = symbolRaw == 0 ? null : symbolLookup[symbolRaw][0];
+        item.symbolDesc = symbolRaw == 0 ? null : symbolLookup[symbolRaw][1];
+        output.weatherItems.push(item);
+    }
+
+    output.gaugeItems = [];
+    for (let i = 0; i < gaugeLen; i++) {
+        var item = {};
+        var rainfallBytes = data.bytes.slice(data.ptr, data.ptr + 4);
+        data.ptr += 4;
+        item.reading = roundTwoDp(new Float32Array(rainfallBytes)[0]);
+        item.readingTimeDiffMins = extractNum(data, 2);
+        output.gaugeItems.push(item);
+    }
+
+    var timeSesReceivedS = extractNum(data, 4);
+    output.timeSesReceived = timeSesReceivedS == 0 ? null : new Date(timeSesReceivedS * 1000);
+    var timeInreachSentS = extractNum(data, 4);
+    output.timeInreachSent = timeInreachSentS == 0 ? null : new Date(timeInreachSentS * 1000);
+
+    return output;
+}
+var example = "uulBZRYlAgQBACwEBARFAAQIADYEAAQ0AAQVABYE4wMrACUKABME4AMdACUpXI8/VQFmZoY/XwCy4EBlu+lBZQ==";
+console.log("input", example)
+console.log("output", decode(example))
