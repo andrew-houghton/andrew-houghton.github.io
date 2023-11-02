3bcdda7e6d4950ab67b79944c4e54e624af942a2
Andrew Houghton <houghtonandrew0@gmail.com>
2023-11-03 08:18:37 +1100

    Add fallback gauges, finish query generation code

--- assets/decode.js
+++ assets/decode.js
@@ -9,7 +9,7 @@ function base64ToArrayBuffer(base64) {
 
 function extractNum(data, numBytes) {
   data.ptr += numBytes;
-  var selectedBytes = data.bytes.slice(data.ptr-numBytes, data.ptr);
+  var selectedBytes = data.bytes.slice(data.ptr - numBytes, data.ptr);
 
   var val;
   if (numBytes == 1) {
@@ -19,7 +19,7 @@ function extractNum(data, numBytes) {
   } else {
     var val = new Uint32Array(selectedBytes)[0];
   }
-  if (val == 2 ** (numBytes*8) - 1) return null;
+  if (val == 2 ** (numBytes * 8) - 1) return null;
   return val;
 }
 
@@ -28,50 +28,50 @@ function roundTwoDp(num) {
 }
 
 var symbolLookup = [
-  ['01d.svg', 'clear sky'],
-  ['02d.svg', 'fair'],
-  ['03d.svg', 'partly cloudy'],
-  ['04.svg', 'cloudy'],
-  ['05d.svg', 'rain showers'],
-  ['06d.svg', 'rain showers and thunder'],
-  ['07d.svg', 'sleet showers'],
-  ['08d.svg', 'snow showers'],
-  ['09.svg', 'rain'],
-  ['10.svg', 'heavy rain'],
-  ['11.svg', 'heavy rain and thunder'],
-  ['12.svg', 'sleet'],
-  ['13.svg', 'snow'],
-  ['14.svg', 'snow and thunder'],
-  ['15.svg', 'fog'],
-  ['20d.svg', 'sleet showers and thunder'],
-  ['21d.svg', 'snow showers and thunder'],
-  ['22.svg', 'rain and thunder'],
-  ['23.svg', 'sleet and thunder'],
-  ['24d.svg', 'light rain showers and thunder'],
-  ['25d.svg', 'heavy rain showers and thunder'],
-  ['26d.svg', 'light sleet showers and thunder'],
-  ['27d.svg', 'heavy sleet showers and thunder'],
-  ['28d.svg', 'light snow showers and thunder'],
-  ['29d.svg', 'heavy snow showers and thunder'],
-  ['30.svg', 'light rain and thunder'],
-  ['31.svg', 'light sleet and thunder'],
-  ['32.svg', 'heavy sleet and thunder'],
-  ['33.svg', 'light snow and thunder'],
-  ['34.svg', 'heavy snow and thunder'],
-  ['40d.svg', 'light rain showers'],
-  ['41d.svg', 'heavy rain showers'],
-  ['42d.svg', 'light sleet showers'],
-  ['43d.svg', 'heavy sleet showers'],
-  ['44d.svg', 'light snow showers'],
-  ['45d.svg', 'heavy snow showers'],
-  ['46.svg', 'light rain'],
-  ['47.svg', 'light sleet'],
-  ['48.svg', 'heavy sleet'],
-  ['49.svg', 'light snow'],
-  ['50.svg', 'heavy snow'],
-]
+  ["01d.svg", "clear sky"],
+  ["02d.svg", "fair"],
+  ["03d.svg", "partly cloudy"],
+  ["04.svg", "cloudy"],
+  ["05d.svg", "rain showers"],
+  ["06d.svg", "rain showers and thunder"],
+  ["07d.svg", "sleet showers"],
+  ["08d.svg", "snow showers"],
+  ["09.svg", "rain"],
+  ["10.svg", "heavy rain"],
+  ["11.svg", "heavy rain and thunder"],
+  ["12.svg", "sleet"],
+  ["13.svg", "snow"],
+  ["14.svg", "snow and thunder"],
+  ["15.svg", "fog"],
+  ["20d.svg", "sleet showers and thunder"],
+  ["21d.svg", "snow showers and thunder"],
+  ["22.svg", "rain and thunder"],
+  ["23.svg", "sleet and thunder"],
+  ["24d.svg", "light rain showers and thunder"],
+  ["25d.svg", "heavy rain showers and thunder"],
+  ["26d.svg", "light sleet showers and thunder"],
+  ["27d.svg", "heavy sleet showers and thunder"],
+  ["28d.svg", "light snow showers and thunder"],
+  ["29d.svg", "heavy snow showers and thunder"],
+  ["30.svg", "light rain and thunder"],
+  ["31.svg", "light sleet and thunder"],
+  ["32.svg", "heavy sleet and thunder"],
+  ["33.svg", "light snow and thunder"],
+  ["34.svg", "heavy snow and thunder"],
+  ["40d.svg", "light rain showers"],
+  ["41d.svg", "heavy rain showers"],
+  ["42d.svg", "light sleet showers"],
+  ["43d.svg", "heavy sleet showers"],
+  ["44d.svg", "light snow showers"],
+  ["45d.svg", "heavy snow showers"],
+  ["46.svg", "light rain"],
+  ["47.svg", "light sleet"],
+  ["48.svg", "heavy sleet"],
+  ["49.svg", "light snow"],
+  ["50.svg", "heavy snow"],
+];
 
-function decode(text){
+function decode(text) {
   // data holds a byte array and a reference to the last position read in the array
   var data = { ptr: 0, bytes: base64ToArrayBuffer(text) };
   var output = {};
@@ -105,9 +105,11 @@ function decode(text){
   }
 
   var timeSesReceivedS = extractNum(data, 4);
-  output.timeSesReceived = timeSesReceivedS == 0 ? null : dayjs.unix(timeSesReceivedS);
+  output.timeSesReceived =
+    timeSesReceivedS == 0 ? null : dayjs.unix(timeSesReceivedS);
   var timeInreachSentS = extractNum(data, 4);
-  output.timeInreachSent = timeInreachSentS == 0 ? null : dayjs.unix(timeInreachSentS);
+  output.timeInreachSent =
+    timeInreachSentS == 0 ? null : dayjs.unix(timeInreachSentS);
   return output;
 }
 
@@ -140,7 +142,7 @@ function render(output) {
   document.getElementById("parsed-data-section").style.display = "block";
 
   var weatherTbody = document.getElementById("weather-forecast-tbody");
-  weatherTbody.innerHTML = '';
+  weatherTbody.innerHTML = "";
   if (output.weatherItems.length == 0) {
     weatherTbody.textContent = "No weather readings";
   } else {
@@ -150,12 +152,14 @@ function render(output) {
       // Date
       var td = tr.insertCell();
       td.classList.add("mdl-data-table__cell--non-numeric");
-      td.appendChild(document.createTextNode(output.baseTime.add(i, 'day').format('D MMM')));
+      td.appendChild(
+        document.createTextNode(output.baseTime.add(i, "day").format("D MMM")),
+      );
 
       // Icon
       var td = tr.insertCell();
       td.classList.add("mdl-data-table__cell--non-numeric");
-      var icon = document.createElement('img');
+      var icon = document.createElement("img");
       icon.src = `/assets/svg/${output.weatherItems[i].symbolUrl}`;
       icon.alt = output.weatherItems[i].symbolDesc;
       icon.height = 40;
@@ -163,20 +167,28 @@ function render(output) {
 
       // Max / Min
       var td = tr.insertCell();
-      td.appendChild(document.createTextNode(`${output.weatherItems[i].max}° / ${output.weatherItems[i].min}°`));
+      td.appendChild(
+        document.createTextNode(
+          `${output.weatherItems[i].max}° / ${output.weatherItems[i].min}°`,
+        ),
+      );
 
       // Precip
       var td = tr.insertCell();
-      td.appendChild(document.createTextNode(`${output.weatherItems[i].rainfall} mm`));
+      td.appendChild(
+        document.createTextNode(`${output.weatherItems[i].rainfall} mm`),
+      );
 
       // Wind
       var td = tr.insertCell();
-      td.appendChild(document.createTextNode(`${output.weatherItems[i].wind} km/h`));
+      td.appendChild(
+        document.createTextNode(`${output.weatherItems[i].wind} km/h`),
+      );
     }
   }
 
   var gaugeTbody = document.getElementById("gauge-readings-tbody");
-  gaugeTbody.innerHTML = '';
+  gaugeTbody.innerHTML = "";
   if (output.gaugeItems.length == 0) {
     gaugeTbody.textContent = "No weather readings";
   } else {
@@ -184,31 +196,47 @@ function render(output) {
       var tr = gaugeTbody.insertRow();
 
       // Date
-      var readingTime = output.baseTime.subtract(output.gaugeItems[i].readingTimeDiffMins, 'minute');
+      var readingTime = output.baseTime.subtract(
+        output.gaugeItems[i].readingTimeDiffMins,
+        "minute",
+      );
       var td = tr.insertCell();
       td.classList.add("mdl-data-table__cell--non-numeric");
-      var dateText = document.createElement('div');
-      dateText.appendChild(document.createTextNode(readingTime.format('D MMM h:m A')));
+      var dateText = document.createElement("div");
+      dateText.appendChild(
+        document.createTextNode(readingTime.format("D MMM h:m A")),
+      );
       td.appendChild(dateText);
-      var relativeText = document.createElement('div');
+      var relativeText = document.createElement("div");
       relativeText.appendChild(document.createTextNode(readingTime.fromNow()));
       td.appendChild(relativeText);
 
       // Reading
       var td = tr.insertCell();
-      td.appendChild(document.createTextNode(readingWithUnits(output.gaugeItems[i].reading, output.gaugeId)));
+      td.appendChild(
+        document.createTextNode(
+          readingWithUnits(output.gaugeItems[i].reading, output.gaugeId),
+        ),
+      );
     }
   }
 
   // Fill text
-  document.getElementById("gauge-name").textContent = `${gaugeData[output.gaugeId].data_source} - ${gaugeData[output.gaugeId].name}`;
+  document.getElementById("gauge-name").textContent = `${
+    gaugeData[output.gaugeId].data_source
+  } - ${gaugeData[output.gaugeId].name}`;
   document.getElementById("gauge-id-debug").textContent = output.gaugeId;
-  document.getElementById("time-ses-received").textContent = output.timeSesReceived.fromNow();
-  document.getElementById("time-ses-received-abs").textContent = output.timeSesReceived.toString();
+  document.getElementById("time-ses-received").textContent =
+    output.timeSesReceived.fromNow();
+  document.getElementById("time-ses-received-abs").textContent =
+    output.timeSesReceived.toString();
   document.getElementById("base-time").textContent = output.baseTime.fromNow();
-  document.getElementById("base-time-abs").textContent = output.baseTime.toString();
-  document.getElementById("time-inreach-sent").textContent = output.timeInreachSent.fromNow();
-  document.getElementById("time-inreach-sent-abs").textContent = output.timeInreachSent.toString();
+  document.getElementById("base-time-abs").textContent =
+    output.baseTime.toString();
+  document.getElementById("time-inreach-sent").textContent =
+    output.timeInreachSent.fromNow();
+  document.getElementById("time-inreach-sent-abs").textContent =
+    output.timeInreachSent.toString();
 }
 
 function parse(text) {
--- assets/search.js
+++ assets/search.js
@@ -1,44 +1,57 @@
-let selectedState = 'all';
-let selectedSearch = '';
+let selectedState = "all";
+let selectedSearch = "";
 let selectedGauge = null;
 
 function selectGauge(key) {
   selectedGauge = key;
   document.getElementById("gauge-search").style.display = "none";
   document.getElementById("gauge-display").style.display = "block";
-  document.getElementById("selected-gauge-text").textContent = gaugeData[selectedGauge].name;
-
+  document.getElementById("selected-gauge-text").textContent =
+    gaugeData[selectedGauge].name;
+  updateRequestText();
 }
+
 function deselectGauge() {
   selectedGauge = null;
   document.getElementById("gauge-search").style.display = "block";
   document.getElementById("gauge-display").style.display = "none";
+  updateRequestText();
 }
 
 function stateFilterSelected(radio) {
-  selectedState = radio.value
+  selectedState = radio.value;
   showGauges();
 }
 
-function debounce(func, timeout = 300){
+function debounce(func, timeout = 300) {
   let timer;
   return (...args) => {
     clearTimeout(timer);
-    timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
   };
 }
 
-function updateSearchString(){
+function updateSearchString() {
   selectedSearch = document.getElementById("search-text").value;
   showGauges();
 }
 
+function updateLocationString() {
+  updateRequestText();
+}
+
 const searchChange = debounce(() => updateSearchString());
+const locationChange = debounce(() => updateLocationString());
 
 function publishLocation(position) {
   var latLonInput = document.getElementById("lat-lon-input");
-  latLonInput.value = `${position.coords.latitude.toFixed(4)},${position.coords.longitude.toFixed(4)}`;
+  latLonInput.value = `${position.coords.latitude.toFixed(
+    4,
+  )},${position.coords.longitude.toFixed(4)}`;
   document.getElementById("lat-lon-input-label").style.display = "none";
+  updateRequestText();
 }
 function loadLocation() {
   if (navigator.geolocation) {
@@ -46,46 +59,105 @@ function loadLocation() {
   }
 }
 
+function copyText() {
+  var requestTextDisplay = document.getElementById("request-text");
+  navigator.clipboard.writeText(requestTextDisplay.textContent.trim());
+}
+
+function updateRequestText() {
+  var locationText = document.getElementById("lat-lon-input").value.trim();
+  var plaintextReply = document.getElementById("plaintext-reply").checked;
+
+  var requestTextDisplay = document.getElementById("request-text");
+  requestTextDisplay.textContent = `gauge:${
+    selectedGauge || "NO GAUGE"
+  } location:${locationText || "NO LOCATION"} client:1${
+    plaintextReply ? " text" : ""
+  }`;
+
+  var valid = true;
+  var statusChipContainer = document.getElementById("error-chip");
+  statusChipContainer.innerHTML = "";
+  var errors = document.getElementById("errors");
+  errors.innerHTML = "";
+
+  if (!selectedGauge) {
+    valid = false;
+    var p = document.createElement("p");
+    p.textContent = `Gauge must be selected.`;
+    errors.appendChild(p);
+  }
+  if (!locationText) {
+    valid = false;
+    var p = document.createElement("p");
+    p.textContent = `Location must be set to 'lat,lon'.`;
+    errors.appendChild(p);
+  } else if (!/^-?[0-9.]+,-?[0-9.]+$/.test(locationText)) {
+    valid = false;
+    var p = document.createElement("p");
+    p.textContent = `Location: '${locationText}' must match format '-23.1236,123.1245'`;
+    errors.appendChild(p);
+  }
+
+  var chip = document.createElement("span");
+  chip.classList.add("mdl-chip");
+  var chipText = document.createElement("span");
+  chipText.classList.add("mdl-chip__text");
+  var copyButton = document.getElementById("copy-button");
+  if (valid) {
+    chipText.textContent = "Ready to send";
+    chip.style.background = "#D2F8D2";
+    copyButton.disabled = false;
+  } else {
+    chipText.textContent = "Invalid";
+    chip.style.background = "#FFDDDB";
+    copyButton.disabled = true;
+  }
+  chip.appendChild(chipText);
+  statusChipContainer.appendChild(chip);
+}
+
 function showGauges() {
   var gaugeGrid = document.getElementById("gauge-grid");
-  gaugeGrid.innerHTML = '';
+  gaugeGrid.innerHTML = "";
   var gaugeList = Object.entries(gaugeData);
 
   // Sort
-  gaugeList.sort((a,b) => a[1].name.localeCompare(b[1].name))
+  gaugeList.sort((a, b) => a[1].name.localeCompare(b[1].name));
 
   // State filter
-  if (selectedState != 'all') {
-    gaugeList = gaugeList.filter((g) => g[1].state == selectedState)
+  if (selectedState != "all") {
+    gaugeList = gaugeList.filter((g) => g[1].state == selectedState);
   }
 
   // Search filter
-  if (selectedSearch != '') {
-    var ss = selectedSearch.toLowerCase();  // case insensitive search
+  if (selectedSearch != "") {
+    var ss = selectedSearch.toLowerCase(); // case insensitive search
 
-    gaugeList = gaugeList.filter((g) => (
-      g[0] == selectedSearch ||
-      g[1].name.toLowerCase().includes(ss) ||
-      g[1].data_source.toLowerCase().includes(ss) ||
-      g[1].sections_strings.some((s) => s.toLowerCase().includes(ss))
-    ));
+    gaugeList = gaugeList.filter(
+      (g) =>
+        g[0] == selectedSearch ||
+        g[1].name.toLowerCase().includes(ss) ||
+        g[1].data_source.toLowerCase().includes(ss) ||
+        g[1].sections_strings.some((s) => s.toLowerCase().includes(ss)),
+    );
   }
 
   // Handle empty results
   var gaugeCount = gaugeList.length;
   if (gaugeCount == 0) {
-    var end_message = document.createElement('p');
-    end_message.textContent = `No gauges.`
+    var end_message = document.createElement("p");
+    end_message.textContent = `No gauges.`;
     gaugeGrid.appendChild(end_message);
   }
 
   // Display items
   for (const [key, value] of gaugeList) {
-    var item = document.createElement('div');
+    var item = document.createElement("div");
     item.classList.add("mdl-cell");
     item.classList.add("mdl-cell--3-col");
 
-    var a = document.createElement('a');
+    var a = document.createElement("a");
     a.textContent = value.name;
     a.style.textWrap = "nowrap";
     a.style.textOverflow = "ellipsis";
@@ -100,14 +172,13 @@ function showGauges() {
 
     // Handle overflow
     if (gaugeGrid.childElementCount >= 20 && gaugeCount > 20) {
-      var end_message = document.createElement('p');
+      var end_message = document.createElement("p");
       end_message.style.margin = "8px";
-      end_message.textContent = `And ${gaugeCount-20} other gauges`;
+      end_message.textContent = `And ${gaugeCount - 20} other gauges`;
       gaugeGrid.appendChild(end_message);
       break;
     }
   }
-
 }
 
 function setupGauges() {
